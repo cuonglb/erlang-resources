3a303634649d8b33f5634022b85fb094f4c42d1f
root <root@cuonglb-desktop.(none)>
2009-06-10 10:59:19 +0700

    * Clean up

--- trunk/xmpp/ejabberd-mod/src/mod_http_auth.erl
+++ trunk/xmpp/ejabberd-mod/src/mod_http_auth.erl
@@ -1,9 +1,17 @@
-%%%-------------------------------------------------------------------
-%%% File    : mod_http_auth.erl
-%%% Author  : Cuong Le <cuonglb@facemain.com>
-%%% Description : HTTP Authentication module for Ejabberd
-%%% Created : 20 Feb 2009 by Cuong.Le <cuonglb@facemain.com>
-%%%-------------------------------------------------------------------
+%% @author Cuong Le <cuonglb@facemain.com>
+%%
+%%    Licensed under the Apache License, Version 2.0 (the "License");
+%%    you may not use this file except in compliance with the License.
+%%    You may obtain a copy of the License at
+%%
+%%        http://www.apache.org/licenses/LICENSE-2.0
+%%
+%%    Unless required by applicable law or agreed to in writing, software
+%%    distributed under the License is distributed on an "AS IS" BASIS,
+%%    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%    See the License for the specific language governing permissions and
+%%    limitations under the License.
+
 -module(mod_http_auth).
 -author('cuonglb@facemain.com').
 
